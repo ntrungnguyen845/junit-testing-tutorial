4392133db3955f834d249ca41d82979e196edad1
TRUNG_NGUYEN <ntrungnguyen845@gmail.com>
2024-03-19 19:16:58 +0700

    fix issues

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,6 +18,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Something like that");
         System.out.println("Thank you so much");
+        System.out.println("researching feature 1");
     }
 
 }
